3d57dd0f3d2e84c4cd6523248c1dacfc2e22b1fb
lochi <72388533+lochidev@users.noreply.github.com>
2021-12-23 10:42:51 +0530

    Add project files.

---
+++ sumatraPDFColorChanger/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace sumatraPDFColorChanger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Pallets = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.ForeColor = System.Drawing.Color.SlateBlue;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.button1.Location = new System.Drawing.Point(113, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Apply pallete";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.MenuText;
+            this.button2.ForeColor = System.Drawing.Color.LightCoral;
+            this.button2.Location = new System.Drawing.Point(75, 199);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "RESET TO DEFAULTS";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Pallets
+            // 
+            this.Pallets.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.Pallets.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Pallets.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Pallets.FormattingEnabled = true;
+            this.Pallets.ItemHeight = 21;
+            this.Pallets.Location = new System.Drawing.Point(129, 32);
+            this.Pallets.Name = "Pallets";
+            this.Pallets.Size = new System.Drawing.Size(194, 88);
+            this.Pallets.TabIndex = 2;
+            this.Pallets.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.ForeColor = System.Drawing.Color.LightSalmon;
+            this.button3.Location = new System.Drawing.Point(249, 147);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(74, 90);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Open config folder";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(453, 266);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Pallets);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.Coral;
+            this.Name = "Form1";
+            this.Text = "SumatraPDFColorChanger";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private ListBox Pallets;
+        private Button button3;
+    }
+}
---
+++ sumatraPDFColorChanger/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+
+namespace sumatraPDFColorChanger
+{
+    public partial class Form1 : Form
+    {
+        private readonly string _filePath;
+        private string _currentConfig = "green-on-blue.txt";
+        public Form1()
+        {
+            string appData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            _filePath = $"{appData}\\SumatraPDF\\SumatraPDF-settings.txt";
+            InitializeComponent();
+            foreach (string? file in Directory.GetFiles("configs"))
+            {
+                Pallets.Items.Add(file[8..]);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string? configText = File.ReadAllText($"configs//{_currentConfig}");
+            File.WriteAllText(_filePath, configText);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(_filePath, string.Empty);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            _currentConfig = (string)Pallets.SelectedItem;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process.Start(Environment.GetEnvironmentVariable("WINDIR") + @"\explorer.exe", $"{Directory.GetCurrentDirectory()}\\configs\\");
+        }
+    }
+}
